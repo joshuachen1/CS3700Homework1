c57eb35a36befb8030e118a7c3ebe83225ef64f3
Joshua Chen <joshuachen1@cpp.edu>
2018-08-28 22:20:48 -0700

    change (2) to hwOne.java

--- hwOne.java
+++ hwOne.java
@@ -1,6 +1,6 @@
 public class hwOne{
 	public static void main(String[] args){
-		System.out.println("Hello, my name is Joshua Chen");
+		System.out.println("Goodbye, I finished my CS 3700 HW1");
 	}
 }
 
